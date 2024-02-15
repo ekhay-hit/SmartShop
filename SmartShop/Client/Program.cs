acc501e305743aa8f5c04d65f4e3907036f0f797
Houssine <ekhay1@outlook.com>
2024-02-14 20:38:14 -0500

    Category service response client side and the implementation of the category in the Navbar home page

--- SmartShop/Client/Program.cs
+++ SmartShop/Client/Program.cs
@@ -1,9 +1,12 @@
 global using System.Net.Http.Json;
+global using SmartShop.Shared;
+global using SmartShop.Client.Services.ProductService;
+global using SmartShop.Client.Services.CategoryService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartShop.Client;
-using SmartShop.Client.Services.ProductService;
-using SmartShop.Shared;
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,4 +14,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 await builder.Build().RunAsync();
