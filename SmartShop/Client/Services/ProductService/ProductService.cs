3a9bebac3586c705468565975ea6cf724ad7245e
Houssine <ekhay1@outlook.com>
2024-02-15 19:56:34 -0500

    Implementing product category in the client side

--- SmartShop/Client/Services/ProductService/ProductService.cs
+++ SmartShop/Client/Services/ProductService/ProductService.cs
@@ -1,31 +1,40 @@
 ﻿using SmartShop.Shared;
 
-
 namespace SmartShop.Client.Services.ProductService
 {
     public class ProductService : IProductService
     {
         private readonly HttpClient _http;
 
+        
+
         public ProductService(HttpClient http)
         {
             _http = http;
         }
         public List<Product> Products { get  ; set ; }= new List<Product>();
-       
 
-        public async Task GetProducts()
-        {
-            var result = 
-                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
-            if (result != null && result.Data != null)
-                Products = result.Data;
-        }
-
+        public event Action ProductsChanged;
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
             return result;
         }
+
+        public async Task GetProducts(string? categoryUrl = null)
+        {
+            var result = categoryUrl == null ?
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product"):
+            await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+                 
+            
+                       ProductsChanged.Invoke();
+        }
+
+       
+
+        
     }
 }
