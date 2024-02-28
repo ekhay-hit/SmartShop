c4fc52aa0c95bb296aef14bad708222f652a36f6
Houssine <ekhay1@outlook.com>
2024-02-28 17:25:14 -0500

    Cart counter implementation

--- SmartShop/Client/Program.cs
+++ SmartShop/Client/Program.cs
@@ -5,6 +5,7 @@ global using SmartShop.Client.Services.CategoryService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartShop.Client;
+using Blazored.LocalStorage;
 
 
 
@@ -12,6 +13,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
