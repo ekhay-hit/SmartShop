5128bea05dff8c8987e8b05e0c0fc4328fd4e8f9
Houssine <ekhay1@outlook.com>
2024-03-01 18:12:17 -0500

    Implementing delete function

--- SmartShop/Client/Services/CartService/ICartService.cs
+++ SmartShop/Client/Services/CartService/ICartService.cs
@@ -7,5 +7,6 @@
         Task AddToCart(CartItem item);
         Task<List<CartItem>> GetCartItems();
         Task<List<CartProductResponse>> GetCartProducts();
+        Task RemoveProductFromCart(int productId, int productTypeId);
     }
 }
