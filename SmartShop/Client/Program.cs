18eed79cc380bb3a09d2070f4a023417d1f46085
Houssine <ekhay1@outlook.com>
2024-03-06 21:03:54 -0500

    edit

--- SmartShop/Client/Program.cs
+++ SmartShop/Client/Program.cs
@@ -4,7 +4,7 @@ global using SmartShop.Client.Services.ProductService;
 global using SmartShop.Client.Services.CategoryService;
 global using SmartShop.Client.Services.CartService;
 global using SmartShop.Client.Services.AuthService;
-global using Microsoft.AspNetCore.Components.Authorization
+global using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartShop.Client;
