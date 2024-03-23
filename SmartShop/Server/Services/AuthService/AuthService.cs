2cebc98ccf4eac356cedf0cb26a4bf8b4fdc839b
Houssine <ekhay1@outlook.com>
2024-03-23 16:15:08 -0400

    Adding a password change Auth

--- SmartShop/Server/Services/AuthService/AuthService.cs
+++ SmartShop/Server/Services/AuthService/AuthService.cs
@@ -129,6 +129,30 @@ namespace SmartShop.Server.Services.AuthService
             return jwt;
         }
 
+        public async Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+            {
+                return new ServiceResponse<bool> { 
+                    success = false,
+                    Message="User not found."
+                };
+
+
+            }
+            CreatePasswordHash(newPassword, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<bool> {
+                Data = true,
+                Message = "Password has been changed."
+            };
+
 
         }
+    }
 }
