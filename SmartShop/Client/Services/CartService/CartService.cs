8f8733ed9cd4da1f59b8f821468d91fb4dc46e1c
Houssine <ekhay1@outlook.com>
2025-01-20 19:35:40 -0500

    maded updted to the front end to add items by calling the backend addToCart

--- SmartShop/Client/Services/CartService/CartService.cs
+++ SmartShop/Client/Services/CartService/CartService.cs
@@ -28,28 +28,28 @@ namespace SmartShop.Client.Services.CartService
             // check if the user is authenticated, if use get cart items from database
             if(await IsUserAuthenticated())
             {
-                Console.WriteLine("User is authenticated");
+                await _http.PostAsJsonAsync("api/cart/add",cartItem);
             }
             else
             {
-                Console.WriteLine("user is NOT authenticated");
-            }
-            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
-            if (cart == null)
-            {
-                cart=new List<CartItem>();
-            }
-            var sameItem = cart.Find(x=> x.ProductId == cartItem.ProductId && x.ProductTypeId== cartItem.ProductTypeId);
-            if (sameItem == null)
-            {
-                cart.Add(cartItem);
-            }
-            else
-            {
-                sameItem.Quantity += cartItem.Quantity;
-            }
+                var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+                if (cart == null)
+                {
+                    cart = new List<CartItem>();
+                }
+                var sameItem = cart.Find(x => x.ProductId == cartItem.ProductId && x.ProductTypeId == cartItem.ProductTypeId);
+                if (sameItem == null)
+                {
+                    cart.Add(cartItem);
+                }
+                else
+                {
+                    sameItem.Quantity += cartItem.Quantity;
+                }
 
-            await _localStorage.SetItemAsync("cart", cart);
+                await _localStorage.SetItemAsync("cart", cart);
+            }
+            
             await GetCartItemsCount();
         }
    
--- SmartShop/Server/Services/CartService/CartService.cs
+++ SmartShop/Server/Services/CartService/CartService.cs
@@ -84,5 +84,24 @@ namespace SmartShop.Server.Services.CartService
            return await GetCartProducts(await _context.CartItems
                .Where(CartItem => CartItem.UserId == GetUserId()).ToListAsync());
         }
+
+        public async Task<ServiceResponse<bool>>AddToCart(CartItem cartItem)
+        {
+            cartItem.UserId = GetUserId();
+            var sameItem = await _context.CartItems
+                .FirstOrDefaultAsync(ci => ci.ProductId == cartItem.ProductId &&
+                ci.ProductTypeId == cartItem.ProductTypeId &&
+                ci.UserId == cartItem.UserId);
+            if(sameItem == null)
+            {
+                _context.CartItems.Add(cartItem);
+            }
+            else
+            {
+                sameItem.Quantity += cartItem.Quantity;
+            }
+            await _context.SaveChangesAsync();
+            return new ServiceResponse<bool> { Data= sameItem };
+        }
     }
 }
