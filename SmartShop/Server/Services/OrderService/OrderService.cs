fe90b72c790f3d13fa64a375bf1dd6177499fd87
Houssine <ekhay1@outlook.com>
2025-01-26 16:27:29 -0500

    adjusted place order request to remove items from cartItem when an order is placed

--- SmartShop/Server/Services/OrderService/OrderService.cs
+++ SmartShop/Server/Services/OrderService/OrderService.cs
@@ -42,7 +42,9 @@ namespace SmartShop.Server.Services.OrderService
                 OrderItems = orderItems
             };
             _context.Orders.Add(order);
+            _context.CartItems.RemoveRange(_context.CartItems.Where(ci => ci.UserId == GetUserId()));
             await _context.SaveChangesAsync();
+           
 
             return new ServiceResponse<bool> { Data = true };
 
