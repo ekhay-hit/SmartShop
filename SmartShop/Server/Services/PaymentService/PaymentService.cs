4af72de18b53a92f0acc55a31e9a1e9a2b7ba03f
Houssine <ekhay1@outlook.com>
2025-01-31 15:00:25 -0500

    implmented the payement service for the backend and added authservice to get the email of the user

--- SmartShop/Server/Services/PaymentService/PaymentService.cs
+++ SmartShop/Server/Services/PaymentService/PaymentService.cs
@@ -1,5 +1,8 @@
 ﻿using SmartShop.Server.Services.CartService;
-using Stripe.BillingPortal;
+using Stripe;
+
+using Stripe.Checkout;
+using Stripe.TestHelpers.Treasury;
 
 namespace SmartShop.Server.Services.PaymentService
 {
@@ -13,13 +16,48 @@ namespace SmartShop.Server.Services.PaymentService
             IOrderService orderService,
             IAuthService authService)
         {
+            StripeConfiguration.ApiKey = "sk_test_51QnPTs2NcxKNi27ddrkLRMNDX5SFmZt785MpF0EonHZw5jSPlSqcNgD2T3pPyXoplQqyVCQNNMpThRgnUlZoKitr00lvNos3w2";
             _cartService = cartService;
             _orderService = orderService;
             _authService = authService;
         }
-        public Task<Session> CreateoutCheckoutSession()
+
+        public async Task<Session> CreateoutCheckoutSession()
         {
-            throw new NotImplementedException();
+            var products = (await _cartService.GetDbCartProducts()).Data;
+            var lineItems = new List<SessionLineItemOptions>();
+            products.ForEach(product => lineItems.Add(new SessionLineItemOptions()
+            {
+                PriceData = new SessionLineItemPriceDataOptions
+                {
+                    UnitAmountDecimal = product.Price * 100,
+                    Currency = "usd",
+                    ProductData = new SessionLineItemPriceDataProductDataOptions
+                    {
+                        Name = product.Title,
+                        Images = new List<string> { product.ImageUrl }
+                    }
+
+                },
+                Quantity = product.Quantity,
+            }));
+            var options = new SessionCreateOptions
+            {
+                CustomerEmail = _authService.GetUserEmail(),
+                PaymentMethodTypes = new List<string>
+                {
+                    "card"
+                },
+                LineItems = lineItems,
+                Mode = "payment",
+                SuccessUrl = "https://localhost:5001/order-success",
+                CancelUrl = "https://localhost:5001/cart"
+
+            };
+            var service = new SessionService();
+            Session session =  service.Create(options);
+            return session;
+
         }
     }
-}
+    }
